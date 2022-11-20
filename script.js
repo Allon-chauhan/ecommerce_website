dbd1ebb541bb0a5eaebf27f303a40a74aa82bbba
Allon-chauhan <chauhanallonashish@gmail.com>
2022-11-20 14:12:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let items = 0;
+function Cart()
+{
+    let total_cost = 0;
+
+    total_cost;
+
+    document.getElementById('totalcost').innerHTML = total_cost;
+    
+}
+
+function total_items()
+{
+
+    items++;
+
+    console.log("Total Items are:" + total_items);
+}
+
+
+
+
+
+function fnButton()
+{
+    alert('Added to the Cart!');
+}
